99864999c2bd26283a3a1e737141fcd9addab0d9
Kurt Bowes <31830401+kbowes99@users.noreply.github.com>
2017-10-13 12:24:33 -0300

    set header

--- src/Driver.java
+++ src/Driver.java
@@ -1,13 +1,8 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ CS 2043 Project - ATM
+    Kurt Bowes, Kody Tippett, Sebastien Gauthier
  */
 
-/**
- *
- * @author lap-kb
- */
 public class Driver {
 
     /**
